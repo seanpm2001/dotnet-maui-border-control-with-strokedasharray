39bc518afdd3814b9500bb8742a99a154619f2bf
Maik Switalski <maik@zauberzeug.com>
2023-05-05 10:50:17 +0200

    cleanup

--- App/MauiProgram.cs
+++ App/MauiProgram.cs
@@ -4,22 +4,11 @@ namespace App;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
-
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
-
-		return builder.Build();
-	}
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>();
+        return builder.Build();
+    }
 }
 
