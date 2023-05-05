5a56726b297955577e859c630c26f4a53034b19d
Maik Switalski <maik@zauberzeug.com>
2023-05-05 10:54:30 +0200

    reproduce layout bug.

--- App/MainPage.xaml.cs
+++ App/MainPage.xaml.cs
@@ -5,6 +5,30 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+        Content = new StackLayout
+        {
+            Spacing = 20,
+            Margin = 10,
+            Children = {
+                new Border {
+                    Stroke = Brush.Black,
+                    StrokeThickness = 3,
+                    Content = new Label { Text = "Without StrokeDashOffset" },
+                    Padding = 10,
+
+                    StrokeDashArray = new DoubleCollection { 2, 1 },
+                    },
+                new Border {
+                    Stroke = Brush.Black,
+                    StrokeThickness = 3,
+                    Content = new Label { Text = "With StrokeDashOffset" },
+                    Padding = 10,
+
+                    StrokeDashArray = new DoubleCollection { 2, 1 },
+                    StrokeDashOffset = 1,
+                },
+            }
+        };
     }
 }
 
